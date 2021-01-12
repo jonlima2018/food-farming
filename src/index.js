baf063196925e5ce373f67f4a4ba81cbcf5de791
jonlima2018 <jonathan_martins_lima@yahoo.com.br>
2021-01-12 20:39:44 -0300

    fix: globalstyle and theme in correct folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
-import GlobalStyle from '../Style/GlobalStyle';
-import theme from '../Style/Theme';
 import App from './App';
+import GlobalStyle from './Style/GlobalStyle';
+import theme from './Style/theme';
 
 ReactDOM.render(
     <React.StrictMode>
