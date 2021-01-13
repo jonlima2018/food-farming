d1f7d64e3ac959804d57b29c875452693d43ee6d
jonlima2018 <jonathan_martins_lima@yahoo.com.br>
2021-01-13 00:55:03 -0300

    feat: add css variables

--- src/Style/theme.js
+++ src/Style/theme.js
@@ -1,6 +1,11 @@
 const theme = {
-    colors: {},
-    font: {},
+    text: '#000000',
+    active: '#C08D5D',
+    menuFont: 'Helvetica, sans-serif',
+    menuBackground: '#ffffff',
+    containerBackground: '#FFFAF3',
+    homeTitleFont: "'Playfair Display', serif",
+    homeTitleText: '#3f4042',
 };
 
 export default theme;
