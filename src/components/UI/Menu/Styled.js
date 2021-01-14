9528f1909f9660c9b448b1506155f02b12b90be5
jonlima2018 <jonathan_martins_lima@yahoo.com.br>
2021-01-13 22:53:21 -0300

    feat(UI/Menu): Create media query for mobile

--- src/components/UI/Menu/Styled.js
+++ src/components/UI/Menu/Styled.js
@@ -8,37 +8,79 @@ const StyledNav = styled.nav`
     align-items: center;
     z-index: 99;
     position: relative;
-    /* background: ${({ theme }) => theme.menuBackground}; */
 
     .nav__menu {
         display: flex;
-    }
 
-    .nav__item {
-        text-transform: capitalize;
-        padding: 15px;
-        margin: 0 10px;
-        font-family: ${({ theme }) => theme.menuFont};
-        font-style: normal;
-        font-weight: 500;
-        font-size: 18px;
-        line-height: 27px;
-        cursor: pointer;
+        .nav__item {
+            text-transform: capitalize;
+            padding: 15px;
+            margin: 0 10px;
+            font-family: ${({ theme }) => theme.menuFont};
+            font-style: normal;
+            font-weight: 500;
+            font-size: 18px;
+            line-height: 27px;
+            cursor: pointer;
 
-        &:hover {
+            &:hover {
+                color: ${({ theme }) => theme.active};
+            }
+        }
+
+        .nav__item--active {
+            font-weight: 800;
             color: ${({ theme }) => theme.active};
         }
     }
 
-    .nav__item--active {
-        font-weight: 800;
-        color: ${({ theme }) => theme.active};
+    .nav__logo {
+        display: none;
     }
 
     .nav__img {
         margin: 0 60px;
         height: 59px;
         width: 38.64px;
+    }
+
+    @media (max-width: 800px) {
+        display: flex;
+        flex-direction: column;
+        width: fit-content;
+        height: fit-content;
+
+        .nav__logo {
+            display: block;
+            margin-top: 20px;
+            margin-left: 30px;
+            height: 59px;
+            width: 38.64px;
+            cursor: pointer;
+        }
+
+        .nav__menu {
+            visibility: hidden;
+            opacity: 0;
+            background: ${({ theme }) => theme.menuBackground};
+            border-radius: 4px;
+            box-shadow: 0px 1px 4px -1px black;
+            flex-direction: column;
+            position: absolute;
+            z-index: 1;
+            top: 85px;
+            left: 5px;
+
+            .nav__item__logo {
+                display: none;
+            }
+        }
+
+        .nav__menu--active {
+            transition: all 0.8s ease-in;
+            visibility: visible;
+            opacity: 1;
+        }
     }
 `;
 
--- src/components/UI/Menu/index.jsx
+++ src/components/UI/Menu/index.jsx
@@ -1,16 +1,26 @@
+import { useState } from 'react';
 import logo from '../../../assets/logo.svg';
 import StyledNav from './Styled';
 
 function Menu() {
+    const [showMenu, setShowMenu] = useState(false);
+
     return (
         <StyledNav>
-            <ul className="nav__menu">
+            <img
+                className="nav__logo"
+                src={logo}
+                alt="Logo"
+                onClick={() => setShowMenu(!showMenu)}
+            />
+
+            <ul className={`nav__menu ${showMenu ? 'nav__menu--active' : ''}`}>
                 <li className="nav__item nav__item--active">Home</li>
                 <li className="nav__item">About</li>
                 <li className="nav__item">Products</li>
-            </ul>
-            <img className="nav__img" src={logo} alt="Logo" />
-            <ul className="nav__menu">
+                <li className="nav__item__logo">
+                    <img className="nav__img" src={logo} alt="Logo" />
+                </li>
                 <li className="nav__item">Recipes</li>
                 <li className="nav__item">FAQ</li>
                 <li className="nav__item">Help</li>
